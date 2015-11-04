89da494f9e391c367a9c8a2aa42bb2c5d1532cee
flow-J <hongjunjie2012@yeah.net>
2015-11-05 05:37:05 +0800

    changes

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+	FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+	FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>' #nedd change  like @163 or qq.com
