57c8c1de8be568fa933c1510698c27048669633c
flow-J <hongjunjie2012@yeah.net>
2015-11-07 07:11:45 +0800

    some code

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin
 from . import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 
 class Role(db.Model):
@@ -20,6 +21,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
 
     @property
     def password(self):
@@ -35,7 +37,11 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+    def generate_confirmation_token(selfself, expiration=3600):
+        s =
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
