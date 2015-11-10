0a9ca28378d859d921804f651b6117cd570792b9
flow-J <hongjunjie2012@yeah.net>
2015-11-11 06:25:26 +0800

    some code

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
@@ -6,3 +7,11 @@ from wtforms.validators import Required
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+
+#资料编辑表单
+class EditProfileForm(Form):
+    name = StringField('Real name', validators=[Length(0,64)])
+    location = StringField('Location', validators=[Length(0,64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('submit')
