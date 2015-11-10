eae91fccfbeb66f0b7787b04dbaf63b266a4e921
flow-J <hongjunjie2012@yeah.net>
2015-11-11 07:19:26 +0800

    some code

--- app/main/forms.py
+++ app/main/forms.py
@@ -15,3 +15,17 @@ class EditProfileForm(Form):
     location = StringField('Location', validators=[Length(0,64)])
     about_me = TextAreaField('About me')
     submit = SubmitField('submit')
+
+
+#资料编辑表单  管理员版
+class EditProfileAdminForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64),
+                                             Email()])
+    username = StringField('Username', validators=[
+        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-Z0-9_.]*$', 0,
+                                          'usernames must have only letters, '
+                                          'numbers, dots or underscores')]) # 点 和 下划线
+    confirmed = BoleanField('Confirmed')
+    role = SelectField('Role', coerce=int)
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])# validators验证器
--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 from flask import render_template, session, redirect, url_for, current_app
 from .. import db
 from ..models import User
@@ -32,3 +33,20 @@ def user(username):
     if user is None:
         abort(404)
     return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET','POST'])
+@login_required
+def edit_profile(): # 资料编辑路由
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data# 位置 当前时间
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('You profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
