c219db60dc2f1b85048252b66c1dcd5bb8ff36d4
flow-J <hongjunjie2012@yeah.net>
2015-11-12 05:53:42 +0800

    some code

--- app/main/views.py
+++ app/main/views.py
@@ -8,11 +8,6 @@ from ..models import Role, User
 from ..decorators import admin_required
 
 
-@main.route('/')
-def index():
-    return render_template('index.html')
-
-
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
@@ -62,3 +57,17 @@ def edit_profile_admin(id):
     form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
+
+
+# 处理博客文章的首页路由
+@main.route('/', methods=['GET','POST'])
+def index():
+    form = PostForm()
+    if current_user.can(Permission.WRITE_ARTICLES) and \
+            form.validate_on_submit():
+        post = Post(body=form.body.data,
+                    author=current_user.__get__current_object())
+        db.session.add(post)
+        return redirect(url_for('.index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts)
