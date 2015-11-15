451ca725ddf119750e9035eb5a9a591483ea6b5b
flow-J <hongjunjie2012@yeah.net>
2015-11-16 02:50:15 +0800

    some code

--- app/models.py
+++ app/models.py
@@ -117,6 +117,7 @@ class User(UserMixin, db.Model):
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = hashlib.md5(
                 self.email.encode('utf-8')).hexdigest()
+        self.follow(self)
 
     @property
     def password(self):
