ad43135b46610c7a61b4b7ac9ab93ff806b5279d
flow-J <hongjunjie2012@yeah.net>
2015-11-16 12:22:41 +0800

    Flask-HTTPAuth 错误处理程序

--- app/api_1_0/authentication.py
+++ app/api_1_0/authentication.py
@@ -5,6 +5,7 @@ __author__ = 'flow'
 
 # 基本验证
 from flask.ext.httpauth import HTTPBasicAuth
+from .errors import forbidden_error
 auth = HTTPBasicAuth()
 
 
@@ -29,3 +30,37 @@ def auth_error():
 @auth.login_required
 def get_posts():
 	pass
+
+
+# Flask-HTTPAuth 错误处理程序
+@api.before_request
+@auth.login_required
+def before_request():
+	if not g.current_user.is_anonymous and \
+			not g.current_user.confirmed:
+		return forbidden('Unconfirmed account')
+
+
+# 支持令牌的改进验证回调
+@auth.verify_password
+def verify_password(email_or_token, password):
+	if email_or_token == '':
+		g.current_user = AnonymousUser()
+		return True
+	if password == '':
+		g.current_user = User.verify_auth_token(email_or_token)
+		g.token_used = True
+		return g.current_user is not None
+	user = User.query.filter_by(email=email_or_token).first()
+	if not user:
+		return False
+	g.current_user = user
+	g.token_used = False
+	return user.verify_password(password)
+
+@api.route('/token')
+def get_token():
+	if g.current_user.is_anonymous() or g.token_used:
+		return unauthorized('Invalid credentials')
+	return jsonify({'token': g.current_user.generate_auth_token(
+		expiration=3600), 'expiration': 3600})
