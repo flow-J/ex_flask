636b089a37c79e95c3c07f0999fbcf7b4b3d0f3e
flow-J <hongjunjie2012@yeah.net>
2015-11-16 13:14:31 +0800

    api

--- app/api_1_0/__init__.py
+++ app/api_1_0/__init__.py
@@ -1,10 +1,5 @@
-# coding: utf-8
-__author__ = 'flow'
 from flask import Blueprint
-
 
 api = Blueprint('api', __name__)
 
-
-# import了各种py文件
 from . import authentication, posts, users, comments, errors
--- app/main/errors.py
+++ app/main/errors.py
@@ -1,16 +1,15 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from . import main
 
 
 @main.app_errorhandler(403)
 def forbidden(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'forbidden'})
+        response.status_code = 403
+        return response
     return render_template('403.html'), 403
-
-
-
-@main.app_errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
 
 
 @main.app_errorhandler(404)
@@ -21,3 +20,13 @@ def page_not_found(e):
         response.status_code = 404
         return response
     return render_template('404.html'), 404
+
+
+@main.app_errorhandler(500)
+def internal_server_error(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'internal server error'})
+        response.status_code = 500
+        return response
+    return render_template('500.html'), 500
